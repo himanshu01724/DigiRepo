fae64584b3e29c290f165a68c2fef1a5c322f199
Himanshu Joshi <hj82641@gmail.com>
2022-03-14 15:38:22 +0530

    screen 5

---
+++ app/src/main/java/com/example/digirepo/Screen5.java
@@ -0,0 +1,14 @@
+package com.example.digirepo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Screen5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen5);
+    }
+}
